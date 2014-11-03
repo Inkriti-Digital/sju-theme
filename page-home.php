fdbe74e582b3dc3995db799cf2427be3ca42ccfd
jkipker <jkipker@gmail.com>
2014-11-03 10:04:37 -0800

    font-artifacts corrected

--- page-home.php
+++ page-home.php
@@ -174,8 +174,8 @@ get_header(); ?>
       </nav>
       <div class="share">
         <h4>Share</h4>
-        <a target="_blank"  href="http://twitter.com/share?text=Look for" target="_blank" class="twit twitterpopup">twitter</a>
-        <a  target="_blank" href="#" onClick="window.open('http://www.facebook.com/sharer/sharer.php?u=http://qa-inkriti-sju.inkriti.net/', 'facebook_share', 'height=320, width=640, toolbar=no, menubar=no, scrollbars=no, resizable=no, location=no, directories=no, status=no');" class="fb">facebook</a>
+        <a  href="http://twitter.com/share?text=Look for" target="_blank" class="twit twitterpopup">twitter</a>
+        <a  href="#" onClick="window.open('http://www.facebook.com/sharer/sharer.php?u=http://qa-inkriti-sju.inkriti.net/', 'facebook_share', 'height=320, width=640, toolbar=no, menubar=no, scrollbars=no, resizable=no, location=no, directories=no, status=no');" class="fb">facebook</a>
       </div>
 </div>
 </section><!-- /wrap -->
